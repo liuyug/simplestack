71c2f8fd1dfe0e4aa3c0f6f63ed62f1cc380a56d
liuyug <liuyug@gmail.com>
2014-05-21 17:52:53 +0800

    Fix

--- network/nova-network_configure.sh
+++ network/nova-network_configure.sh
@@ -17,6 +17,6 @@ nova secgroup-list
 # permit icmp
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
 # permit ssh
-nova  secgroup-add-rule default tcp 22 22 0.0.0.0/16
+nova  secgroup-add-rule default tcp 22 22 0.0.0.0/0
 
 # vim: ts=4 sw=4 et tw=79
